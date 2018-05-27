8b893fc01753e2db981ca8033b985e3262867905
Linus Heckemann <git@sphalerite.org>
2018-05-27 14:57:54 +0000

    Generate and print a map

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+extern crate rand;
+use rand::{thread_rng, Rng};
+use rand::seq::sample_slice;
+use rand::distributions::{Uniform, Binomial};
 /* 
    Map<Player, Planet>
    Map<Player, Fleet>
@@ -36,6 +40,8 @@ struct Game {
     players: Vec<Player>,
     fleets: Vec<Fleet>,
     current_player_index: usize,
+    w: u32,
+    h: u32,
 }
 
 enum CouldNotSend {
@@ -96,18 +102,62 @@ impl Game {
         players: Vec<Player>,
         neutral_planets: u32,
     ) -> Result<Game, CouldNotCreateGame> {
-        if players.len() as u32 + neutral_planets > w * h {
+        let total_planets = players.len() as u32 + neutral_planets;
+        if total_planets > w * h {
             return Err(CouldNotCreateGame::TooManyPlanets);
         }
-        let planets = Vec::new();
+        let mut planets = Vec::new();
+        let mut rng = thread_rng();
+        let mut available_positions: Vec<Pos> = Vec::new();
+        for x in 0..w {
+            for y in 0..h {
+                available_positions.push((x, y));
+            }
+        }
+        let mut positions = sample_slice(&mut rng, &available_positions, total_planets as usize);
+        for (id, player) in players.iter().enumerate() {
+            planets.push(Planet {
+                ships: 0,
+                strength: 40,
+                production: 10,
+                pos: positions.pop().expect("Not enough positions!?"),
+                owner: Some(id),
+            });
+        }
+        let strength_distribution = Binomial::new(100, 0.55);
+        let production_distribution = Binomial::new(10, 0.5);
+        positions.drain(..).map(|pos| Planet {
+            ships: 0,
+            strength: rng.sample(strength_distribution) as u32,
+            production: rng.sample(production_distribution) as u32 + 5,
+            pos: pos,
+            owner: None,
+        }).for_each(|p| planets.push(p));
         Ok(Game {
             planets: planets,
             players: players,
             fleets: Vec::new(),
             current_player_index: 0,
+            w: w,
+            h: h,
         })
+    }
+
+    pub fn print(&self) {
+        for y in 0..self.h {
+            for x in 0..self.w {
+                print!("{}",
+                self.planets.iter()
+                            .filter(|p| p.pos == (x, y))
+                            .take(1).last()
+                            .map(|p| "O")
+                            .unwrap_or(" ")
+                            );
+            }
+            println!("")
+        }
     }
 }
 fn main() {
-    println!("Hello, world!");
+    Game::new(10, 5, vec![Player {}, Player {}, Player {}], 5).map(|g| g.print());
 }
